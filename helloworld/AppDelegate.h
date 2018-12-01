b5404cfc1886bac743b828fbf876ba5ca855962d
junjie58msp <412512621@qq.com>
2018-12-01 21:59:28 +0800

    Initial Commit

---
+++ helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by jayjayli on 2018/12/1.
+//  Copyright © 2018 jayjayli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helloworld
+//
+//  Created by jayjayli on 2018/12/1.
+//  Copyright © 2018 jayjayli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
